6e32575b512ee16041a9b184bff9cf411ef532d6
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <robinborg.pro@gmail.com>
2024-06-04 17:10:06 +0200

    cadebug

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,8 +4,9 @@ class Booking < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :chatrooms, dependent: :destroy
 
-  validates :duration, :token_cost, :session_format, :start_date, :end_date, :content, :status, presence: true
+  validates :token_cost, :session_format, :start_date, :end_date, :content, :status, presence: true
   validates :status, inclusion: { in: %w[pending confirmed rejected] }
 
 
 end
+
