707d92ec9485f7a9cb56f388917b0c942e17f2ad
Mattieu Yves <mattieuyves@gmail.com>
2024-06-05 17:48:05 +0200

    stimulus

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -1,7 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="dashboard"
 export default class extends Controller {
+  static targets = ["student", "teacher"]
+
   connect() {
+  }
+
+  displayStudent() {
+    this.studentTarget.classList.remove("d-none")
+    this.teacherTarget.classList.add("d-none")
+  }
+
+  displayTeacher() {
+    this.studentTarget.classList.add("d-none")
+    this.teacherTarget.classList.remove("d-none")
   }
 }
