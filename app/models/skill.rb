7535ce15723b31be4371f1969024641876f598a4
Jeya Jeyababu <jeya2196@hotmail.fr>
2024-06-05 17:10:46 +0200

    searchbar fait

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,12 +3,18 @@ class Skill < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
-  validates :name, :goal, :experience_year, :category, presence: :true, allow_blank: false
-  validates :experience_year, numericality: { only_integer: true }
-
   CATEGORY = %w[art/artisanat musique lecture écriture cuisine/pâtisserie jardinage photographie randonnée/camping sports jeux/de/société puzzles modélisme danse pêche informatique méditation/yoga observation/oiseaux collection cinéma voyages bricolage tricot/couture poterie/céramique plongée/surf escalade astronomie équitation aéromodélisme calligraphie origami magie cosplay jeux/vidéo dégustation/vins brassage/bières histoire/archéologie langues/étrangères aquariophilie volontariat scrapbooking/digital drone/pilotage]
 
+  validates :name, :goal, :experience_year, :category, presence: :true, allow_blank: false
+  validates :experience_year, numericality: { only_integer: true }
   validates :category, inclusion: CATEGORY, presence: true
 
+
+  include PgSearch::Model
+  pg_search_scope :search_by_skill,
+    against: [ :skill ],
+    using: {
+      tsearch: { prefix: true }
+    }
 
 end
