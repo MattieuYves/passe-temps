dd4b9e4229bcbb4b3f74095f6f283e3cba5eaadf
Jean Motte <jijimotte@gmail.com>
2024-06-06 16:36:11 +0200

    fix seed so that fix update delete method on booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,10 @@
 class Booking < ApplicationRecord
   after_create :create_chatroom
+  has_one :chatroom, dependent: :destroy
 
   belongs_to :skill
   belongs_to :user
   has_many :reviews, dependent: :destroy
-  has_one :chatroom, dependent: :destroy
 
   validates :token_cost, :session_format, :start_date, :end_date, :content, :status, presence: true
   validates :status, inclusion: { in: %w[pending confirmed rejected] }
