cc6d01a1073546d68a14b6c53af840f70928abb2
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <robinborg.pro@gmail.com>
2024-06-07 12:45:20 +0200

    correct some bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, :set_chatrooms
+  before_action :authenticate_user!
+  before_action :set_chatrooms, if: :user_signed_in?
   private
 
   def set_chatrooms
     @chatrooms = current_user.chatrooms
   end
+  
+
 end
