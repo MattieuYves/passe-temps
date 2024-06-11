d867e742f765c8cb8e7d582f956075fdb3cdea22
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <robinborg.pro@gmail.com>
2024-06-11 17:21:11 +0200

    a

---
+++ app/javascript/controllers/aos_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+export default class extends Controller {
+  connect() {
+    AOS.init();
+  }
+}
