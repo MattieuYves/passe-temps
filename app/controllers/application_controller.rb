efe56ce26b58ca9232e56bc392460f0bcdc68ec0
Jean Motte <jijimotte@gmail.com>
2024-06-12 11:08:59 +0200

    meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :set_chatrooms, if: :user_signed_in?
   private
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   def set_chatrooms
     @chatrooms = current_user.chatrooms
   end
-  
+
 
 end
